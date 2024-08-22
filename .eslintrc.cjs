d0513d5e3a01ff18fe2eac0e059ea18b8b062fd9
James Whaley <juicy66173@gmail.com>
2024-08-22 13:51:27 +0100

    Fix eslint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -80,7 +80,7 @@ module.exports = {
     {
       files: ["*.cjs"],
       rules: {
-        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/no-require-imports": ["error", { allowAsImport: true }],
       },
     },
     {
